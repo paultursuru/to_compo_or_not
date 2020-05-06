2fb18cf98d15a45c9699662a5ae9db5a7219012c
Jennifer Stephan <jennifer.stephan@edu.escpeurope.eu>
2020-05-06 12:51:14 +0200

    ajout de donnees f&l"

--- src/infro-json.js
+++ src/infro-json.js
@@ -72,6 +72,221 @@ const Information = [
     "emoji":"🌱🇧🇪"
   },
   {
+    "label":"Chou-fleur",
+    "season": [0, 1, 2, 8, 9, 10, 11],
+    "emoji":"🌱🌸"
+  },
+  {
+    "label":"Concombre",
+    "season": [4, 5, 6, 7, 8, 9],
+    "emoji":"🌱"
+  },
+  {
+    "label":"Courge",
+    "season": [0, 8, 9, 10, 11],
+    "emoji":"🌱"
+  },
+  {
+    "label":"Courgette",
+    "season": [4, 5, 6, 7, 8, 9],
+    "emoji":"🌱"
+  },
+  {
+    "label":"Cresson",
+    "season": [0, 1, 2, 3, 4, 5, 6, 7, 8, 8, 10, 11],
+    "emoji":"🌱"
+  },
+  {
+    "label":"Echalote",
+    "season": [9, 10, 11],
+    "emoji":"🌱"
+  },
+  {
+    "label":"Endive",
+    "season": [0, 1, 2, 3, 4, 9],
+    "emoji":"🌱"
+  },
+  {
+    "label":"Epinard",
+    "season": [0, 1, 2, 3, 4, 8, 9, 10, 11],
+    "emoji":"🌱"
+  },
+  {
+    "label":"Fenouil",
+    "season": [3, 5, 6, 7, 8, 9, 10],
+    "emoji":"🌱"
+  },
+  {
+    "label":"Haricot vert",
+    "season": [5, 6, 7, 8, 9],
+    "emoji":"🌱"
+  },
+  {
+    "label":"Laitue",
+    "season": [4, 5, 6, 7, 8],
+    "emoji":"🌱"
+  },
+  {
+    "label":"Mâche",
+    "season": [0, 1, 9, 10, 11],
+    "emoji":"🌱"
+  },
+  {
+    "label":"Navet",
+    "season": [0, 1, 2, 3, 4, 9, 10, 11],
+    "emoji":"🌱"
+  },
+  {
+    "label":"Maïs",
+    "season": [6, 7, 8],
+    "emoji":"🌽"
+  },
+  {
+    "label":"Oignon",
+    "season": [0, 1, 2, 3, 8, 9, 10, 11],
+    "emoji":"🌱"
+  },
+  {
+    "label":"Panais",
+    "season": [0, 1, 2, 9, 10, 11],
+    "emoji":"🌱"
+  },
+  {
+    "label":"Petit pois",
+    "season": [4, 5, 6],
+    "emoji":"🌱"
+  },
+  {
+    "label":"Poireau",
+    "season": [0, 1, 2, 3, 4, 5, 6, 7, 8, 8, 10, 11],
+    "emoji":"🌱"
+  },
+  {
+    "label":"Poivron",
+    "season": [5, 6, 7, 8],
+    "emoji":"🌱"
+  },
+  {
+    "label":"Potiron",
+    "season": [0, 8, 9, 10, 11],
+    "emoji":"🎃"
+  },
+  {
+    "label":"Radis",
+    "season": [2, 3, 4, 5],
+    "emoji":"🌱"
+  },
+  {
+    "label":"Salsifis",
+    "season": [0, 1, 2, 10, 11],
+    "emoji":"🌱"
+  },
+  {
+    "label":"Topinambour",
+    "season": [0, 1, 10, 11],
+    "emoji":"🌱"
+  },
+  {
+    "label":"Cassis",
+    "season": [5, 6, 7],
+    "emoji":"🌱"
+  },
+  {
+    "label":"Châtaigne",
+    "season": [9, 10],
+    "emoji":"🌰"
+  },
+  {
+    "label":"Clémentine",
+    "season": [0, 1, 10, 11],
+    "emoji":"🍊"
+  },
+  {
+    "label":"Pamplemousse",
+    "season": [1, 2, 3, 4, 5],
+    "emoji":"🍊"
+  },
+  {
+    "label":"Coing",
+    "season": [9],
+    "emoji":"🌱"
+  },
+  {
+    "label":"Figue",
+    "season": [6, 7, 8, 9],
+    "emoji":"🌱"
+  },
+  {
+    "label":"Groseille",
+    "season": [5, 6, 7],
+    "emoji":"🌱"
+  },
+  {
+    "label":"Kiwi",
+    "season": [0, 1, 2, 10, 11],
+    "emoji":"🌱"
+  },
+  {
+    "label":"Mandarine",
+    "season": [0, 1, 10, 11],
+    "emoji":"🍊"
+  },
+  {
+    "label":"Melon",
+    "season": [5, 6, 7, 8, 9],
+    "emoji":"🍈"
+  },
+  {
+    "label":"Mirabelle",
+    "season": [7, 8],
+    "emoji":"🌱"
+  },
+  {
+    "label":"Mûre",
+    "season": [7, 8],
+    "emoji":"🌱"
+  },
+  {
+    "label":"Nectarine",
+    "season": [7, 8],
+    "emoji":"🌱"
+  },
+  {
+    "label":"Myrtille",
+    "season": [7, 8, 9],
+    "emoji":"🌱"
+  },
+  {
+    "label":"Noisette",
+    "season": [8, 9, 10],
+    "emoji":"🌰"
+  },
+  {
+    "label":"Noix",
+    "season": [8, 9],
+    "emoji":"🌰"
+  },
+  {
+    "label":"Prune",
+    "season": [6, 7, 8],
+    "emoji":"🌱"
+  },
+  {
+    "label":"Quetsche",
+    "season": [7, 8, 9],
+    "emoji":"🌱"
+  },
+  {
+    "label":"Reine Claude",
+    "season": [8],
+    "emoji":"🌱"
+  },
+  {
+    "label":"Rhubarbe",
+    "season": [3, 4, 5],
+    "emoji":"🌱"
+  },
+  {
     "label":"Pêche",
     "season": [5, 6, 7, 8],
     "emoji":"🍑"
